45d692d21a9f3ccabb54dad3d496c9275c39bb6c
saifsammy <saifsammy653@gmail.com>
2023-03-16 15:04:23 +0600

    submit button works successfully

--- index.js
+++ index.js
@@ -53,15 +53,13 @@ const loadQuiz = async () => {
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
-  console.log(data);
   if (!data) {
     quizContainer.innerHTML = "";
     return;
   }
 
   data.forEach((quiz, i) => {
-    console.log(quiz);
-    const { question, options, description }=quiz
+    const { question, options}=quiz
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -77,7 +75,8 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
+  console.log('submit button kaj kore na');
   if (answers.length < 6) {
     return;
   }
